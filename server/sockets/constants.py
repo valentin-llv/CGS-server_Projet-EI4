979558eed8d19dc7f38673b821b4033347813dc4
Thibault HILAIRE <thibault.hilaire@lip6.fr>
2025-02-27 14:55:30 +0100

    when the name is local, `socket.gethostbyname` may raise an error (the case on my machine where `socket.gethostname()` returns `"mont-d-or"`)

--- server/sockets/constants.py
+++ server/sockets/constants.py
@@ -1,7 +1,11 @@
 import socket
 
 # Socket server
-SERVER_ADR = socket.gethostbyname(socket.gethostname())
+try:
+    SERVER_ADR = socket.gethostbyname(socket.gethostname())
+except socket.gaierror:
+    SERVER_ADR = '127.0.0.1'
+
 
 #SERVER_ADR = "0.0.0.0"
 SERVER_PORT = 15001
